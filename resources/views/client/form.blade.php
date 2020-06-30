95a9a231dd637e3df4dfa8506bf7acb9a2a52c7b
ypearl84 <y.pearl84@gmail.com>
2020-06-29 23:26:35 -0700

    commit 2020.06.29.05

--- resources/views/client/form.blade.php
+++ resources/views/client/form.blade.php
@@ -16,33 +16,41 @@
           <div class="medium-4  columns">
             <label>Name</label>
             <input name="name" type="text">
+            <small class="error">{{$errors->first('name')}}</small>
           </div>
           <div class="medium-4  columns">
             <label>Last Name</label>
             <input name="last_name" type="text">
+            <small class="error">{{$errors->first('last_name')}}</small>
           </div>
           <div class="medium-8  columns">
             <label>Address</label>
             <input name="address" type="text">
+            <small class="error">{{$errors->first('address')}}</small>
           </div>
           <div class="medium-4  columns">
             <label>zip_code</label>
             <input name="zip_code" type="text">
+            <small class="error">{{$errors->first('zip_code')}}</small>
           </div>
           <div class="medium-4  columns">
             <label>City</label>
             <input name="city" type="text">
+            <small class="error">{{$errors->first('city')}}</small>
           </div>
           <div class="medium-4  columns">
             <label>State</label>
             <input name="state" type="text">
+            <small class="error">{{$errors->first('state')}}</small>
           </div>
           <div class="medium-12  columns">
             <label>Email</label>
             <input name="email" type="text">
+            <small class="error">{{$errors->first('email')}}</small>
           </div>
           <div class="medium-12  columns">
             <input value="SAVE" class="button success hollow" type="submit">
+            
           </div>
         </form>
       </div>
